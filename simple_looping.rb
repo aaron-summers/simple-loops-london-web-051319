6db79025429b647bc9354fe9d1f5c6088de9adcf
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-02 23:02:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,11 +6,11 @@ def loop_iterator(number_of_times)
   loop do 
     counter += 1
     puts phrase
-    break if counter > number_of_times 
-      
+    if counter > number_of_times 
+      break
     end
   end
-# end
+end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
