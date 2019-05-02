447272d0db440785b61f47d50d39d67dcac1e99e
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-02 22:33:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,9 +13,8 @@ def loop_iterator(number_of_times)
   loop do 
     counter += 1
     puts phrase
-    break if 
-    
-  
+    break if counter < number_of_times
+  end
 end
 
 def times_iterator(number_of_times)
